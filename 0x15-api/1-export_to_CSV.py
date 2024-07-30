0003915cd88c2eef985ed93a539a963cc9606af6
Mohamed Arrafouf <107930069+rrmeed@users.noreply.github.com>
2024-07-30 12:30:59 +0100

    Add files via upload

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Reads todo list from api for employee id passed'''
+'''Reads todo list from api for id passed'''
 
+import csv
 import requests
 import sys
 
@@ -9,7 +10,8 @@ base_url = 'https://jsonplaceholder.typicode.com/'
 
 def do_request():
     '''Performs request'''
-    if len(sys.argv) < 2:
+
+    if not len(sys.argv):
         return print('USAGE:', __file__, '<employee id>')
     eid = sys.argv[1]
     try:
@@ -28,14 +30,17 @@ def do_request():
     if response.status_code != 200:
         return print('Error: status_code:', response.status_code)
     todos = response.json()
-
     user_todos = [todo for todo in todos
                   if todo.get('userId') == user.get('id')]
     completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
+
+    with open(eid + '.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, lineterminator='\n',
+                            quoting=csv.QUOTE_ALL)
+        [writer.writerow(['{}'.format(field) for field in
+                          (todo.get('userId'), user.get('username'),
+                           todo.get('completed'), todo.get('title'))])
+         for todo in user_todos]
 
 if __name__ == '__main__':
     do_request()
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Reads todo list from api for employee id passed'''
+'''Reads todo list from api for id passed and turns into json file'''
 
+import json
 import requests
 import sys
 
@@ -9,6 +10,7 @@ base_url = 'https://jsonplaceholder.typicode.com/'
 
 def do_request():
     '''Performs request'''
+
     if len(sys.argv) < 2:
         return print('USAGE:', __file__, '<employee id>')
     eid = sys.argv[1]
@@ -28,14 +30,17 @@ def do_request():
     if response.status_code != 200:
         return print('Error: status_code:', response.status_code)
     todos = response.json()
-
     user_todos = [todo for todo in todos
                   if todo.get('userId') == user.get('id')]
     completed = [todo for todo in user_todos if todo.get('completed')]
-    print('Employee', user.get('name'),
-          'is done with tasks({}/{}):'.
-          format(len(completed), len(user_todos)))
-    [print('\t', todo.get('title')) for todo in completed]
+
+    user_todos = [{'task': todo.get('title'),
+                   'completed': todo.get('completed'),
+                   'username': user.get('username')}
+                  for todo in user_todos]
+    data = {eid: user_todos}
+    with open(eid + '.json', 'w') as file:
+        json.dump(data, file)
 
 if __name__ == '__main__':
     do_request()
